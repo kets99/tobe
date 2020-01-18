6387b74ed7cf6cae2e080e0d0b7fc0235083ecfe
Ketki Davda <ketki.davda@somaiya.edu>
2020-01-18 06:41:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var app = express();
 // var exphbs = require('express-handlebars');
 
 // var handlebars = require('express-handlebars');
-app.use(express.static(path.join(__dirname,"assets")));
+app.use(express.static(path.join(__dirname,"")));
 // app.use(require('cookie-parser')());
 //app.use(require('body-parser').urlencoded({ extended: true }));
 
